014f33c6477d8b435d65f9ecc123d39d17a4d883
andreas riedmann <andreas.riedmann@htldornbirn.at>
2026-03-05 09:05:09 +0100

    test2

--- main.ts
+++ main.ts
@@ -6,6 +6,6 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log("Server running on http://localhost:8000");
+console.log("Server running on http://localhost:8000!");
 
 await app.listen({ port: 8000 });
